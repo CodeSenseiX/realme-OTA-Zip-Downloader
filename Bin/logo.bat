fe479cba6f9fbe75a3a7f7db77ff1562ca975d26
CodeSenseiX <abhinavftp53@gmail.com>
2025-01-26 04:49:10 +0000

    update logo.bat

--- Bin/logo.bat
+++ Bin/logo.bat
@@ -1,26 +1,10 @@
-
-:: Define the bold PSG
-set "P1=	__________"
-set "P2=	\______   \"
-set "P3=	 |     ___/"
-set "P4=	 |    |    "
-set "P5=	 |____|    "
-set "P6=	           "
-
-set "S1=  _________"
-set "S2= /   _____/"
-set "S3= \_____  \ "
-set "S4= /        \"
-set "S5=/_______  /"
-set "S6=        \/ "
-
-set "G1=  ________ "
-set "G2= /  _____/ "
-set "G3=/   \  ___ "
-set "G4=\    \_\  \"
-set "G5= \______  /"
-set "G6=        \/ "
-
+:: Define the bold CSX with the new logo
+set "C1=   _________            .___       _________               _________      .______  ___  "
+set "C2=  \_   ___ \  ____   __| _/____  /   _____/ ____   ____  /   _____/ ____ |__\   \/  /  "
+set "C3= /    \  \/ /  _ \ / __ |/ __ \ \_____  \_/ __ \ /    \ \_____  \_/ __ \|  |\     /    "
+set "C4= \     \___(  <_> ) /_/ \  ___/ /        \  ___/|   |  \/        \  ___/|  |/     \   "
+set "C5=  \______  /\____/\____ |\___  >_______  /\___  >___|  /_______  /\___  >__/___/\  \  "
+set "C6=         \/            \/    \/        \/     \/     \/        \/     \/         \_/   "
 
 :: Slide the text
 for /l %%i in (10,-1,0) do (
@@ -36,26 +20,23 @@ for /l %%i in (10,-1,0) do (
     echo.
     echo.
     echo.
-    echo                 !P1:~%%i! !S1:~%%i! !G1:~%%i!
-    echo                 !P2:~%%i! !S2:~%%i! !G2:~%%i!
-    echo                 !P3:~%%i! !S3:~%%i! !G3:~%%i!
-    echo                 !P4:~%%i! !S4:~%%i! !G4:~%%i!
-    echo                 !P5:~%%i! !S5:~%%i! !G5:~%%i!
-    echo                 !P6:~%%i! !S6:~%%i! !G6:~%%i!
+    echo                 !C1:~%%i!
+    echo                 !C2:~%%i!
+    echo                 !C3:~%%i!
+    echo                 !C4:~%%i!
+    echo                 !C5:~%%i!
+    echo                 !C6:~%%i!
     ping 127.0.0.1 -n 1 -w 200 >nul
 )
 
-    echo.
-    echo.
-    echo.
-    echo.
-    echo.
-    echo.
-    echo.
-    echo.
-    echo.
-    echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
 echo					                               ^< ^< ^< ^< ^< ^< ^< ^<
-
-
-
